22eb5a25127f0cd9ef524db44ec70b4497d423b6
Ali Nazari <ali.nazari@aut.ac.ir>
2018-01-21 23:21:52 +0330

    "basic linked list"

--- main.c
+++ main.c
@@ -1,89 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-FILE * AVAILABLE_TOPICS;
-FILE * selected_topic;
+FILE * AVAILABLE_TOPICS ;
+FILE * selected_topic ;
 
 struct node
 {
-    char word[30];
-    struct node *next;
+    char word[30] ;
+    struct node *next ;
 };
 
 int menu()
 {
-    printf("Enter [1] to play a new game\nAnd also enter [2] to resume your previous game:");
-    int char_input;
-    scanf("%d",&char_input);
-    if(char_input==1)
+    printf ( "Enter [1] to play a new game\nAnd also enter [2] to resume your previous game:" ) ;
+    int char_input ;
+    scanf ( "%d", &char_input ) ;
+    if ( char_input == 1 )
     {
         new_game();
     }
-    if(char_input==2)
+    if ( char_input == 2 )
     {
 
     }
     else
     {
-        printf("Wrong input!\n");
-        menu();
+        printf ( "Wrong input!\n" ) ;
+        menu() ;
     }
 }
 
 int new_game()
 {
-    AVAILABLE_TOPICS = fopen("AVAILABLE_TOPICS.txt","r");
-    char c[500];
-    int i=1;
-    while(fgets(c,500,AVAILABLE_TOPICS)!= NULL)
+    AVAILABLE_TOPICS = fopen ( "AVAILABLE_TOPICS.txt" , "r" ) ;
+    char c[500] ;
+    int i = 1 ;
+    while ( fgets( c , 500 , AVAILABLE_TOPICS ) != NULL )
     {
-        printf("Enter [%d] for:  ",i);
-        printf("%s\n",c);
-        i++;
+        printf ( "Enter [%d] for:  " , i ) ;
+        printf ( "%s\n" , c ) ;
+        i++ ;
     }
-    int topic_input;
-    printf("So: ");
-    scanf("%d",&topic_input);
-    open_topic(topic_input);
+    int topic_input ;
+    printf ( "So: " ) ;
+    scanf ( "%d" , &topic_input ) ;
+    open_topic ( topic_input ) ;
 }
 
-int open_topic(int a)
+int open_topic ( int a )
 {
-    switch(a)
+    switch ( a )
     {
-    case 1:
-        selected_topic = fopen("fruits.txt","r");
-    case 2:
-        selected_topic = fopen("soccer_players.txt","r");
-    case 3:
-        selected_topic = fopen("programming_languages.txt","r");
-    case 4:
-        selected_topic = fopen("computer_networks.txt","r");
-    case 5:
-        selected_topic = fopen("video_games.txt","r");
+    case 1 :
+        selected_topic = fopen ( "fruits.txt" , "r" ) ;
+    case 2 :
+        selected_topic = fopen ( "soccer_players.txt" , "r" ) ;
+    case 3 :
+        selected_topic = fopen ( "programming_languages.txt" , "r" ) ;
+    case 4 :
+        selected_topic = fopen ( "computer_networks.txt" , "r" ) ;
+    case 5 :
+        selected_topic = fopen ( "video_games.txt" , "r" ) ;
     }
-    init_link(selected_topic);
+    init_link ( selected_topic ) ;
 }
 
-int init_link(FILE *b)
+extern int init_link ( FILE *b )
 {
+    struct node *head = ( struct node* )malloc( sizeof ( struct node ) ) ;
+    head->next = NULL;
     struct node *current = head;
-    char c_word;
-    while(feof(b)==0)
+    char c_word ;
+    for( current ; feof ( b ) == 0 ; add_link_node( current ) , current = current->next , current->next = NULL )
     {
-        fscanf(b,"%s",current->word);
-
+        fscanf( b , "%s" , current->word ) ;
     }
+    current = head;
+    while(current->next!=NULL)
+    {
+        printf("%s\n",current->word);
+        current = current->next;
+    }
+}
+
+int add_link_node ( struct node *current )
+{
+    struct node *temp = current ;
+    temp->next = ( struct node* )malloc( sizeof ( struct node ) ) ;
 }
 int main()
 {
-    printf("Hello Friend...Hello Friend!\n");
-    printf("This is a Hangman game.\nLet's play.\n\n");
-    printf("Enter your name:");
-    char char_name[20];
-    scanf("%s",char_name);
-    printf("\nWelcome %s!\n\n",char_name);
-    menu();
+    printf ( "Hello Friend...Hello Friend!\n" ) ;
+    printf ( "This is a Hangman game.\nLet's play.\n\n" ) ;
+    printf ( "Enter your name:" ) ;
+    char char_name[20] ;
+    scanf ( "%s" , char_name ) ;
+    printf ( "\nWelcome %s!\n\n" , char_name ) ;
+    menu() ;
 
-    return 0;
+    return 0 ;
 }
