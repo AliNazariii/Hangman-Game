0e0870c4a38dc3de03efb839962fa859c1073f79
Ali Nazari <ali.nazari@aut.ac.ir>
2018-01-21 13:21:09 +0330

    "rooz aval"

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int char_input;
-FILE * file_topic;
+FILE * AVAILABLE_TOPICS;
+FILE * selected_topic;
+
+struct node
+{
+    char word[30];
+    struct node *next;
+};
 
 int menu()
 {
     printf("Enter [1] to play a new game\nAnd also enter [2] to resume your previous game:");
+    int char_input;
     scanf("%d",&char_input);
     if(char_input==1)
     {
@@ -25,14 +32,47 @@ int menu()
 
 int new_game()
 {
-    file_topic = fopen("AVAILABLE_TOPICS.txt","r+");
+    AVAILABLE_TOPICS = fopen("AVAILABLE_TOPICS.txt","r");
     char c[500];
     int i=1;
-    while(fgets(c,500,file_topic)!= NULL)
+    while(fgets(c,500,AVAILABLE_TOPICS)!= NULL)
     {
         printf("Enter [%d] for:  ",i);
         printf("%s\n",c);
         i++;
+    }
+    int topic_input;
+    printf("So: ");
+    scanf("%d",&topic_input);
+    open_topic(topic_input);
+}
+
+int open_topic(int a)
+{
+    switch(a)
+    {
+    case 1:
+        selected_topic = fopen("fruits.txt","r");
+    case 2:
+        selected_topic = fopen("soccer_players.txt","r");
+    case 3:
+        selected_topic = fopen("programming_languages.txt","r");
+    case 4:
+        selected_topic = fopen("computer_networks.txt","r");
+    case 5:
+        selected_topic = fopen("video_games.txt","r");
+    }
+    init_link(selected_topic);
+}
+
+int init_link(FILE *b)
+{
+    struct node *current = head;
+    char c_word;
+    while(feof(b)==0)
+    {
+        fscanf(b,"%s",current->word);
+
     }
 }
 int main()
@@ -45,7 +85,5 @@ int main()
     printf("\nWelcome %s!\n\n",char_name);
     menu();
 
-
-    //hey i learnt how to use git hahahaha
     return 0;
 }
