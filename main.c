12ad465a5073781670d162274c66940607b666fe
Ali Nazari <ali.nazari@aut.ac.ir>
2018-01-27 04:03:34 +0330

    "mostly finished...midnight"

--- main.c
+++ main.c
@@ -12,9 +12,24 @@ FILE * graphics ;
 FILE * greeting ;
 FILE * correct ;
 FILE * wrong ;
+FILE * main_menu ;
+FILE * not_working ;
+FILE * bye ;
 
 time_t t ;
-//define the structure of nodes
+
+    /*
+    clock_t  begin= clock() //process begins
+
+    process here
+
+    clock_t end= clock //process ends
+
+    time_spent = end-begin/CLOCKS_PER_SEC
+
+    //define the structure of nodes
+    */
+
 struct node
 {
     char word [ 30 ] ;
@@ -33,6 +48,7 @@ typedef struct
 player now ;
 //
 
+void print_file ( FILE * a ) ; //print the content of a file completely !
 int menu () ; //this is my main menu
 int new_game () ; //show topics and select one of them //when the player press 1 we will come here
 int open_topic ( int a ) ; //here we open the selected topic
@@ -51,29 +67,62 @@ void init_players () ;  //get the name of the player and welcoming
 
 int main ()
 {
-
     t = time( NULL ) ;
     srand ( t ) ;
 
     greeting = fopen ( "greeting.txt" , "r" ) ;
-    char ccc [ 500 ] ;
-    while ( fgets ( ccc , 500 , greeting ) != NULL )
-    {
-        printf ( "%s" , ccc ) ;
-    }
+    print_file ( greeting ) ;
+    fclose ( greeting ) ;
 
     init_players () ;
 
     return 0 ;
 }
 
-int menu () //this is my main menu
+void print_file ( FILE * a )
+{
+    char qqq [ 500 ] ;
+    while ( fgets ( qqq , 500 , a ) != NULL )
+    {
+        printf ( "%s" , qqq ) ;
+    }
+}
+
+void init_players ()
+{
+    players = fopen ( "players.txt" , "a+" ) ;
+
+    //get player's name and welcoming
+    printf ( "\t\tEnter your name:" ) ;
+    scanf ( "%s" , now.name ) ;
+    fprintf ( players , "\n%s" , now.name ) ;
+    printf ( "\n\t\tWelcome %s!\n\n" , now.name ) ;
+    Sleep ( 700 ) ;
+    //
+
+    int finish = 1 ;
+    now.sum_score = 0 ;
+    while ( finish )
+    {
+        finish = menu ( now ) ;
+    }
+
+    fprintf ( players , " %d" , now.sum_score ) ;
+    //fprintf ( players , " %d" , now.high_score ) ;
+
+}
+
+int menu ()
 {
     system ( "cls" ) ;
     //show the description of menu and get what player wants
-    printf ( "\nEnter [1] to play a new game\nAnd also enter [2] to resume your previous game\nand you can enter [3] to finish the game:" ) ;
+    main_menu = fopen ( "main_menu.txt" , "r" ) ;
+    print_file ( main_menu ) ;
+    printf ( "So: " ) ;
     int char_input ;
     scanf ( "%d", & char_input ) ;
+    fclose ( main_menu ) ;
+    system ( "cls" ) ;
     //
 
     // if player choose 1, we will start new game with all of the topics and words
@@ -87,24 +136,32 @@ int menu () //this is my main menu
     //if player choose 2, we will continue his previous game
     if ( char_input == 2 )
     {
-
+        not_working = fopen ( "not_working.txt" , "r" ) ;
+        print_file ( not_working ) ;
+        Sleep ( 2500 ) ;
+        fclose ( not_working ) ;
     }
 
     //if player choose 3, we will finish the game
     if ( char_input == 3 )
     {
+        Beep ( 1500 , 500 ) ;
+        bye = fopen ( "bye.txt" , "r" ) ;
+        print_file ( bye ) ;
+        Sleep ( 2500 ) ;
+        fclose ( bye ) ;
         return 0 ;
     }
     //
 
     else //yeah, this is nothing ha ha
     {
-        printf ( "Wrong input!\n" ) ;
+        Beep ( 1000 , 500 ) ;
         menu () ;
     }
 }
 
-int new_game () //show topics and select one of them //when the player press 1 we will come here
+int new_game ()
 {
     AVAILABLE_TOPICS = fopen ( "AVAILABLE_TOPICS.txt" , "r" ) ; //open file of topics
     system( "cls" ) ;
@@ -112,13 +169,14 @@ int new_game () //show topics and select one of them //when the player press 1 w
     //put each line in a string and print each topic
     char c [ 500 ] ;
     int i = 1 ;
+    printf ( "\n" ) ;
     while ( fgets ( c , 500 , AVAILABLE_TOPICS ) != NULL )
     {
-        printf ( "Enter [%d] for:  " , i ) ;
+        printf ( "\tEnter [%d] for:  " , i ) ;
         printf ( "%s\n" , c ) ;
         i++ ;
     }
-    printf ( "\nAnd also you can enter [20] to generate your own topic.\n" ) ;
+    printf ( "\n\tAnd also you can enter [20] to generate your own topic.\n" ) ;
     //
 
     // here we ask player to choose a topic
@@ -131,7 +189,7 @@ int new_game () //show topics and select one of them //when the player press 1 w
     fclose ( AVAILABLE_TOPICS ) ;
 }
 
-int open_topic ( int a ) //here we open the selected topic
+int open_topic ( int a )
 {
     char name_own_topic [ 40 ] ;
     int number_generated ;
@@ -171,7 +229,7 @@ int open_topic ( int a ) //here we open the selected topic
         selected_topic = fopen ( "test.txt" , "r" ) ;
         break ;
     case 20 :
-        printf ( "Enter the name of your topic ( in format: name.txt) : " ) ;
+        printf ( "Enter the name of your topic ( in format: name.txt ) : " ) ;
         scanf ( "%s" , name_own_topic ) ;
         getchar () ;
         selected_topic = fopen ( name_own_topic , "a" ) ;
@@ -195,7 +253,7 @@ int open_topic ( int a ) //here we open the selected topic
     fclose ( selected_topic ) ;
 }
 
-int init_link ( FILE * b )  //put each word into a node of linked list
+int init_link ( FILE * b )
 {
     head = ( struct node* )malloc( sizeof ( struct node ) ) ;
     head -> next = NULL;
@@ -236,7 +294,7 @@ int init_link ( FILE * b )  //put each word into a node of linked list
     //until here
 
 
-    current = head ;
+ /*   current = head ;
     while (1)
     {
         printf("%s\n",current->word);
@@ -247,11 +305,12 @@ int init_link ( FILE * b )  //put each word into a node of linked list
         current = current -> next ;
     }
     printf("\n%d\n",length_link(head));
+ */
 
     process_topic () ;
 }
 
-void add_link_node ( struct node * current ) //add a new node at the end of the linked list
+void add_link_node ( struct node * current )
 {
     current -> next = ( struct node * ) malloc ( sizeof ( struct node ) ) ;
 }
@@ -277,21 +336,43 @@ void process_topic ()
         delete_node ( random ) ; //delete that node of word
     }
     printf ( "Sum of your scores in this topic is: %d" , now.sum_score ) ;
-    Sleep ( 1500 ) ;
+    Sleep ( 2000 ) ;
 }
 
-int true_or_false ( char c , char guessed_char ) //to check whether this character is in this word or not
+int find_random_word ( int length_link )
 {
-    if ( guessed_char == c )
+    return ( ( ( rand () ) % length_link ) + 1 ) ;
+}
+
+int length_link ( struct node * head )
+{
+    struct node * temp = head ;
+    int j = 1 ;
+    while ( temp -> next != NULL )
     {
-        return 1 ;
+        temp = temp -> next ;
+        j++ ;
     }
-    return 0 ;
+    return j ;
+}
+
+void init_word ( int random , char random_word [] )
+{
+    int y ;
+    struct node * selected_node = head ;
+    //go to a loop to reach the selected node
+    for ( y = 1 ; y < random ; y++ )
+    {
+        selected_node = selected_node -> next ;
+    }
+    //
+
+    strcpy ( random_word , selected_node -> word ) ; //copy the word to the array
 }
 
 int process_word_begin ( char random_word [] )
 {
-    int word_length = strlen( random_word ) ;
+    int word_length = strlen ( random_word ) ;
     int p , o ;
     int l = 0 ;
 
@@ -302,7 +383,7 @@ int process_word_begin ( char random_word [] )
     }
     system( "cls" ) ;
     printf( "\nSo the word is like this: " ) ;
-    print_words( print_word , word_length ) ;
+    print_words ( print_word , word_length ) ;
     printf( "\n\n" ) ;
     graphic ( l ) ;
     printf ( "\n" ) ;
@@ -318,7 +399,11 @@ int process_word_begin ( char random_word [] )
         scanf ( "%c", & guessed_char ) ;
         if ( guessed_char == 'P' )
         {
-
+            Beep ( 1500 , 500 ) ;
+            bye = fopen ( "bye.txt" , "r" ) ;
+            print_file ( bye ) ;
+            Sleep ( 2500 ) ;
+            fclose ( bye ) ;
             exit ( -1 ) ;
         }
         if ( guessed_char == 'Q' )
@@ -343,13 +428,12 @@ int process_word_begin ( char random_word [] )
         if ( error == word_length )
         {
             wrong = fopen ( "no.txt" , "r" ) ;
-            char ccccc [ 500 ] ;
-            while ( fgets ( ccccc , 500 , wrong ) != NULL )
-            {
-                printf ( "%s" , ccccc ) ;
-            }
+            Beep ( 1000 , 500 ) ;
+            print_file ( wrong ) ;
             fclose ( wrong ) ;
-            Sleep ( 1000 ) ;
+            Sleep ( 750 ) ;
+            system ( "cls" ) ;
+            printf ( "Wrong!" ) ;
             l++ ;
             switch ( l )
             {
@@ -384,11 +468,7 @@ int process_word_begin ( char random_word [] )
         else
         {
             correct = fopen ( "yes.txt" , "r" ) ;
-            char cccc [ 500 ] ;
-            while ( fgets ( cccc , 500 , correct ) != NULL )
-            {
-                printf ( "%s" , cccc ) ;
-            }
+            print_file ( correct ) ;
             fclose ( correct ) ;
             Sleep ( 1000 ) ;
         }
@@ -404,14 +484,32 @@ int process_word_begin ( char random_word [] )
             now.sum_score += score ;
             printf ( "Score of this round is: %d" , score ) ;
             printf ( "\n\nSum of your scores until now, is: %d" , now.sum_score ) ;
-            printf ( "\n_______________________________________________________\n" ) ;
+            printf ( "\n" ) ;
             Sleep ( 3000 ) ;
             break ;
         }
     }
 }
 
-void graphic ( int l ) // print the process of going the person down the water
+void print_words ( char print_word [] , int word_length )
+{
+    int q ;
+    for ( q = 0 ; q < word_length ; q++ )
+    {
+        printf( " %c" , print_word [ q ] ) ;
+    }
+}
+
+int true_or_false ( char c , char guessed_char )
+{
+    if ( guessed_char == c )
+    {
+        return 1 ;
+    }
+    return 0 ;
+}
+
+void graphic ( int l )
 {
     switch ( l )
     {
@@ -434,38 +532,11 @@ void graphic ( int l ) // print the process of going the person down the water
         graphics = fopen ( "step5.txt" , "r" ) ;
         break ;
     }
-    char cc [ 500 ] ;
-    while ( fgets ( cc , 500 , graphics ) != NULL )
-    {
-        printf ( "%s" , cc ) ;
-    }
+    print_file ( graphics ) ;
     fclose ( graphics ) ;
 }
 
-void print_words ( char print_word [] , int word_length ) //print the word , character by character
-{
-    int q ;
-    for ( q = 0 ; q < word_length ; q++ )
-    {
-        printf( " %c" , print_word [ q ] ) ;
-    }
-}
-
-void init_word ( int random , char random_word [] ) //go to the node and copy the word to an array to process
-{
-    int y ;
-    struct node * selected_node = head ;
-    //go to a loop to reach the selected node
-    for ( y = 1 ; y < random ; y++ )
-    {
-        selected_node = selected_node -> next ;
-    }
-    //
-
-    strcpy( random_word , selected_node -> word ) ; //copy the word to the array
-}
-
-void delete_node ( int random )  // delete the node that we have played with it
+void delete_node ( int random )
 {
     struct node * deleted_node = head ;
     struct node * last = head ;
@@ -506,43 +577,4 @@ void delete_node ( int random )  // delete the node that we have played with it
     }
 }
 
-int find_random_word ( int length_link ) //find a random number to select that node of words
-{
-    return ( ( ( rand () ) % length_link ) + 1 ) ;
-}
 
-int length_link ( struct node * head ) //find the length of the linked list
-{
-    struct node * temp = head ;
-    int j = 1 ;
-    while ( temp -> next != NULL )
-    {
-        temp = temp -> next ;
-        j++ ;
-    }
-    return j ;
-}
-
-void init_players ()  //get the name of the player and welcoming
-{
-    players = fopen ( "players.txt" , "a+" ) ;
-
-    //get player's name and welcoming
-    printf ( "\t\tEnter your name:" ) ;
-    scanf ( "%s" , now.name ) ;
-    fprintf ( players , "\n%s" , now.name ) ;
-    printf ( "\n\t\tWelcome %s!\n\n" , now.name ) ;
-    Sleep ( 1000 ) ;
-    //
-
-    int finish = 1 ;
-    now.sum_score = 0 ;
-    while ( finish )
-    {
-        finish = menu ( now ) ;
-    }
-
-    fprintf ( players , "\t%d" , now.sum_score ) ;
-    //fprintf ( players , "\n%d" , now.high_score ) ;
-
-}
